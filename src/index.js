0bd66f35d7f483b8504d82215b6bb7d1dc8bd59b
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-10-07 21:40:12 +0300

    more then 5 tests pass

--- src/index.js
+++ src/index.js
@@ -1,43 +1,25 @@
 module.exports = function count(s, pairs) {
+  
   const N = pairs.reduce((a, b) => a * Math.pow(b[0], b[1]), 1);
-  const kSet = new Set();
-  if(!isFinite(N)) N=Number.MAX_SAFE_INTEGER;
+  let answer = 0, val;
+  if (!isFinite(N)) return -1;
 
-  for(let i = 1; i <= N; i++){
-    for(let j = 0; j < s.length; j++){
-      let n = 0;
-      switch(s[j]){
-        case '0':
-          if(!checkDivider(i + j, pairs)) break;
-          else n++;
-        case '1':
-          if(checkDivider(i + j, pairs)) break;
-          else n++;
-          
-      }
-      if(n===s.length){
-        kSet.add(i);
-      }
+  
+  for (let k = 0; k <= N; k++) {
+    for (let j = 0; j < s.length; j++) {
+      val = ( getCommonDiviser(k + j, N) === 1 ) ? 1 : 0;
+      check = ( val === +s[j] ) ? true : false;
+      if (!check) break;
     }
-      /*if((s[j]==='1' && !checkDivider(i + j, pairs)) || (s[j]==='0' && checkDivider(i + j, pairs))){
-        kSet.add(i);
-		//console.log(i)
-      }
-      else {
-        break;
-      } */  
+      if (check) answer++;
+    }
+  
+    const result = answer % 1000000007;
+    return result;
   }
   
- function checkDivider(k, dividerArr){
-   for(let i = 0; i < dividerArr.length; i++){
-     if(k%dividerArr[i][0]===0) return true;
-   }
-   return false;
+  
+  function getCommonDiviser(a, b) {
+    if (b == 0) return a;
+    return getCommonDiviser(b, a % b);
   }
-
-
-  return kSet.size%1000000007;
-
-
- 
-}
