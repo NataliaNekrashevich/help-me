6e0f0b75d89a2fe56559ebe2c9a29ccc6018e125
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-10-07 22:10:59 +0300

    Task done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 module.exports = function count(s, pairs) {
-  
+  let answer = 0, check, val;
   const N = pairs.reduce((a, b) => a * Math.pow(b[0], b[1]), 1);
-  let answer = 0, val;
-  if (!isFinite(N)) return -1;
-
-  
-  for (let k = 0; k <= N; k++) {
+  if(!isFinite(N)) return -1;
+  for (let k = 0; k < N; k++) {
     for (let j = 0; j < s.length; j++) {
       val = ( getCommonDiviser(k + j, N) === 1 ) ? 1 : 0;
       check = ( val === +s[j] ) ? true : false;
       if (!check) break;
     }
-      if (check) answer++;
-    }
-  
-    const result = answer % 1000000007;
-    return result;
-  }
+    if (check) answer++;
+  } 
+  const result = answer % 1000000007;
+  return result;
+}
   
   
   function getCommonDiviser(a, b) {
