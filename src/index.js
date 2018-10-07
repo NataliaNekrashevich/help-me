d8d44f6d52a5016d410c8b2eac79aaad1377f244
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-10-07 23:07:14 +0300

    do optimized

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ module.exports = function count(s, pairs) {
   if (N > 100000000) return 0;
   for (var k = 0; k < N; k++) {
     for (var j = 0; j < s.length; j++) {
-      val = ( getCommonDiviser(k + j, N) === 1 ) ? 1 : 0;
+      val = ( checkDivider(k + j, pairs) === 1 ) ? 1 : 0;
       check = ( val === +s[j] ) ? true : false;
       if (!check) break;
     }
     if (check) answer++;
   }
-  const result = answer % 1000000007;
+  const result = answer %   1000000007;
   return result;
 }
 
 
-function getCommonDiviser(a, b) {
-  if (b == 0) return a;
-  return getCommonDiviser(b, a % b);
-}
+function checkDivider(k, dividerArr){
+  for(let i = 0; i < dividerArr.length; i++){
+    if(k%dividerArr[i][0]===0) return 0;
+  }
+  return 1;
+ }
